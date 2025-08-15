dc0f44ca0f277a40c9a02ccfdca38f6bd3555d5f
Brayan Machado <roctemcode@gmail.com>
2025-08-15 00:28:44 -0500

    Se identifica y corrigen errores

---
+++ README.md
@@ -0,0 +1,25 @@
+examenProductoM1Back
+Curso
+- Línea 5 -> Se completa @Entity
+- Línea 6 -> Se agrega el @Table(name = "curso")
+- Línea 9 -> Se completa @Id
+- Línea 10 -> Se completa @GeneratedValue (strategy = GenerationType.IDENTITY)
+- Línea 12 -> Se agrega el punto y coma (;)
+- Línea 15 -> se le quita el punto y coma (;)
+- Línea 17 -> Se agrega el punto y coma (;)
+- A partir de la línea 28 Se agregan Getters y Setters
+
+Docente
+
+- Línea 7 -> Se completa @Entity
+- Línea 8 -> Se agrega el @Table(name = "docente")
+- A partir de la Línea 27 -> se agrega el constructor vacío, el lleno y se completan los getters y setters
+
+Usuario
+
+- Línea 5 -> Se completa @Entity
+- Línea 10 -> se agrega IDENTITY
+- Línea 14 y 17 -> se corrige @Column
+- Línea 19, 33, 37 -> se corrige contraseña a contrasena
+- Línea 22 -> se crea Enum admin, docente y usuario
+- A partir de la línea 42 -> se crean getters y setters
--- src/main/java/com/example/Examen1Back2/modelos/Usuario.java
+++ src/main/java/com/example/Examen1Back2/modelos/Usuario.java
@@ -2,22 +2,26 @@ package com.example.Examen1Back2.modelos;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
-@Entit
+@Entity
 @Table(name = "usuarios")
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.)
-    @Colun(name = "id_usuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
     private Integer id;
 
     @Column(nullable = false, length = 100)
     private String nombre;
 
-    @Colun(name = "correo_electronico", unique = true)
+    @Column(name = "correo_electronico", unique = true)
     private String correoElectronico;
-    private String contraseña;
+    private String contrasena;
     private String telefono;
+
+    public enum TipoUsuario {
+        ADMIN, DOCENTE, USUARIO
+    }
 
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
@@ -27,17 +31,70 @@ public class Usuario {
     @JsonBackReference(value = "docente-usuario")
     private Docente docente;
 
-    public Usuario() {
-    }
+    public Usuario() {}
 
-    public Usuario(Integer id, String nombre, String correoElectronico, String contraseña, String telefono, TipoUsuario tipoUsuario) {
+    public Usuario(Integer id, String nombre, String correoElectronico, String contrasena, String telefono, TipoUsuario tipoUsuario) {
         this.id = id;
         this.nombre = nombre;
         this.correoElectronico = correoElectronico;
-        this.contraseña = contraseña;
+        this.contrasena = contrasena;
         this.telefono = telefono;
         this.tipoUsuario = tipoUsuario;
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContraseña() {
+        return contrasena;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contrasena = contraseña;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
 }
